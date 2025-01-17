a5b504c821b01c2d7c20120bb125a28d7c1b93e5
tonismacbook <jdada@ggc.edu>
2025-01-17 14:14:49 -0500

    Integrating server into application

--- backend/src/main/java/org/example/backend/Dummy.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend;
-
-public class Dummy {
-    public static void main(String[] args) {
-        System.out.println(Dummy.class.getClassLoader().getResource("keystore.p12"));
-    }
-}
