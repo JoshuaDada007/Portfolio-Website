4cd427016cc27f959be5f3cdbec13e364911222e
tonismacbook <jdada@ggc.edu>
2025-01-21 18:28:14 -0500

    Deployed

--- frontend/src/components/layout/NavButton.jsx
+++ frontend/src/components/layout/NavButton.jsx
@@ -1,29 +1,35 @@
 import '../../styles/dashboard.css'
+import {Link} from "react-router";
 
-export function NavButton(){
-    function homeClick() {
-        window.location.href = "/dashboard"
-    }
-    function skillsCLick() {
-        window.location.href = "/skill"
-    }
-
-    function contactsClick() {
-        window.location.href = "/contact"
-    }
-
-    function projectsClick() {
-        window.location.href = "/project"
-    }
+export function NavButton() {
+    // function homeClick() {
+    //     window.location.href = "/dashboard"
+    // }
+    // function skillsCLick() {
+    //     window.location.href = "/skill"
+    // }
+    //
+    // function contactsClick() {
+    //     window.location.href = "/contact"
+    // }
+    //
+    // function projectsClick() {
+    //     window.location.href = "/project"
+    // }
 
 
     return (
-        <div className = "nav-container">
-            <button  onClick={homeClick}>Home</button>
-            <button onClick={skillsCLick}>Skills</button>
-            <button  onClick={projectsClick}>Projects</button>
-            <button  onClick={contactsClick}>Contact</button>
+        <div className="nav-container">
+            {/*<button  onClick={homeClick}>Home</button>*/}
+            {/*<button onClick={skillsCLick}>Skills</button>*/}
+            {/*<button  onClick={projectsClick}>Projects</button>*/}
+            {/*<button  onClick={contactsClick}>Contact</button>*/}
+            <Link className="nav-links" to="/dashboard">Home</Link>
+            <Link className="nav-links" to="/skill">Skills</Link>
+            <Link className="nav-links" to="/project">Projects</Link>
+            <Link className="nav-links" to="/contact">Contact</Link>
 
         </div>
     )
+
 }
